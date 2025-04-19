a24481464d953a1a992ad7a040d343d53e5e0795
vaceslavkusenko <SlavaKushch@yandex.ru>
2025-04-19 06:42:46 +0300

    frontend update, начал страничку создания заказа

--- frontend/src/pages/main/create/CreatePage.jsx
+++ frontend/src/pages/main/create/CreatePage.jsx
@@ -1,6 +1,7 @@
 import Header from "../../../components/Header/Header";
 import Sidebar from "../../../components/Sidebar/Sidebar";
 import React from "react";
+import styles from "./CreatePage.module.css";
 
 const CreatePage = () => {
     return (
@@ -12,9 +13,14 @@ const CreatePage = () => {
                 <div className="content-header">
                     <div className="text">Create Mailing</div>
                 </div>
-            </div>
-        </div>
-    );
-};
+                <div className={styles.content}>
+                    <div className={styles.card}>
 
-export default CreatePage;
+                    </div>
+                </div>
+                </div>
+            </div>
+            );
+            };
+
+            export default CreatePage;
