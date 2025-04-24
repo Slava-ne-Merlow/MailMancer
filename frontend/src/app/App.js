f77faf98e38a0d5e3f842a82c99a0335ba7edc7d
vaceslavkusenko <SlavaKushch@yandex.ru>
2025-04-24 22:54:54 +0300

    Тык

--- frontend/src/app/App.js
+++ frontend/src/app/App.js
@@ -3,7 +3,7 @@ import {BrowserRouter as Router, Routes, Route, Navigate} from "react-router-dom
 import RegisterPage from "../pages/auth/register/RegisterPage";
 import LoginPage from "../pages/auth/login/LoginPage";
 import MailingsPage from "../pages/main/mailings/MailingsPage";
-import TeamPage from "../pages/main/teams/TeamPage";
+import TeamPage from "../pages/main/team/TeamPage";
 import CarriersPage from "../pages/main/carriers/CarriersPage";
 import CreatePage from "../pages/main/create/CreatePage";
 import ProtectedRoute from "../components/ProtectedRoute/ProtectedRoute";
